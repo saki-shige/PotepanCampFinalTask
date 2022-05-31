ed98571a986e3fef3a291bf54255a955fd20a94b
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-05-31 14:37:14 +0900

    商品詳細に関するテストを追加

---
+++ spec/requests/potepan/products_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Potepan::Products", type: :request do
+  before do
+    @product_a = FactoryBot.create(:product)
+    get potepan_product_path(@product_a.id)
+  end
+
+  describe '詳細ページテスト' do
+    it "商品詳細ページから正常にレスポンスを返す" do
+      expect(response).to have_http_status(200)
+    end
+
+    it '商品名が表示される' do
+      expect(response.body).to include(@product_a.name)
+    end
+
+    it '値段が表示される' do
+      expect(response.body).to include(@product_a.display_price.to_s)
+    end
+
+    it '説明が表示される' do
+      expect(response.body).to include(@product_a.description)
+    end
+  end
+end
