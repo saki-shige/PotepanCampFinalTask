5ce5cd4bd7643edd8c88a65e44618acfea15d210
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-03 22:47:44 +0900

    prefixを使用

--- spec/requests/potepan/samples_spec.rb
+++ spec/requests/potepan/samples_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Samples", type: :request do
-  describe "GET /potepan/index" do
+  describe "トップページテスト" do
     it "正常にレスポンスを返す" do
-      get "/potepan/index"
+      get potepan_path
       expect(response).to have_http_status(200)
     end
   end
