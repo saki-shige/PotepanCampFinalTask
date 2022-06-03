f887bcbffbf540b3d8a89ce8a6359eb7d660e30b
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-03 22:43:03 +0900

    不要なコメント削除

--- spec/requests/potepan/samples_spec.rb
+++ spec/requests/potepan/samples_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Samples", type: :request do
-  # describe "GET /potepan/samples" do
-  #   it "works! (now write some real specs)" do
-  #     get potepan_samples_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
-
   describe "GET /potepan/index" do
     it "正常にレスポンスを返す" do
       get "/potepan/index"
