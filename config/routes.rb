77a1ae497c159abdbfa671a36446d18834248802
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-03 20:49:31 +0900

    namespaceブロック統一

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platf
   mount Spree::Core::Engine, at: '/'
 
   namespace :potepan do
-    resources :products, only: [:show]
-  end
-
-  namespace :potepan do
     get '/',                        to: 'sample#index'
     get 'index',                    to: 'sample#index'
     get :product_grid_left_sidebar, to: 'sample#product_grid_left_sidebar'
@@ -29,6 +25,7 @@ mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platf
     get :about_us,                  to: 'sample#about_us'
     get :tokushoho,                 to: 'sample#tokushoho'
     get :privacy_policy,            to: 'sample#privacy_policy'
+    resources :products, only: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
