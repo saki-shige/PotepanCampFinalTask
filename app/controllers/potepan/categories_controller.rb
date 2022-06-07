c623039ed7a5d59104cfdc506fde3d07320f13ba
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-07 23:54:19 +0900

    カテゴリー検索機能の実装

---
+++ app/controllers/potepan/categories_controller.rb
@@ -0,0 +1,7 @@
+class Potepan::CategoriesController < ApplicationController
+  def show
+    @taxon = Spree::Taxon.find(params[:id])
+    @taxonomies = Spree::Taxonomy.all
+    @products = @taxon.products
+  end
+end
