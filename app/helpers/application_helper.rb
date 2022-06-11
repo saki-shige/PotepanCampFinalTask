7ffa16ba4ba185091ac714c5be70f80aa436e2c0
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-11 16:15:40 +0900

    遷移元urlの取得変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,18 +8,4 @@ module ApplicationHelper
       "#{title} - #{DEFAULT_TITLE}"
     end
   end
-
-  def pre_action
-    pre_url[:action]
-  end
-
-  def pre_controller
-    pre_url[:controller]
-  end
-
-  private
-
-  def pre_url
-    Rails.application.routes.recognize_path(request.referrer)
-  end
 end
