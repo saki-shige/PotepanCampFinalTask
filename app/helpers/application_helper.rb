adba9a6c22755c5d8f237bceb526ad64b5e762f7
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-13 10:30:43 +0900

    遷移先の条件分岐をヘルパーに分割

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
       "#{title} - #{DEFAULT_TITLE}"
     end
   end
+
+  def link_destination(product)
+    if /.+potepan\/categories\/\d+$/.match(request.referer)
+      :back
+    elsif taxon_id = product.taxons.first&.id
+      potepan_category_path(taxon_id)
+    else
+      potepan_path
+    end
+  end
 end
