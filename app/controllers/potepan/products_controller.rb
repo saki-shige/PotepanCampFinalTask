4c3705b805ab541bad3382070530b2ede3f3b846
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-15 17:51:10 +0900

    変数名修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -7,13 +7,15 @@ class Potepan::ProductsController < ApplicationController
   private
 
   def list_up_related_products(origin_product)
-    list_taxonomies = Spree::Taxonomy.all.order(position: "ASC").ids
-    list_taxons = origin_product.taxons.order([Arel.sql('field(taxonomy_id, ?)'), list_taxonomies]).includes(:products)
-    list_of_products = []
+    list_taxonomy_ids = Spree::Taxonomy.all.order(:position).ids
+    list_taxons = origin_product.taxons.order([Arel.sql('field(taxonomy_id, ?)'), list_taxonomy_ids]).
+      includes(:products)
+    list_product_ids = []
     list_taxons.each do |list_taxon|
-      list_of_products.concat(list_taxon.products.ids.reverse!)
+      list_product_ids.concat(list_taxon.products.ids.reverse!)
     end
-    list_of_products.uniq!&.delete(origin_product.id)
-    Spree::Product.where(id: list_of_products).order([Arel.sql('field(id, ?)'), list_of_products]).includes(master: [:default_price, :images])
+    list_product_ids.uniq!&.delete(origin_product.id)
+    Spree::Product.where(id: list_product_ids).order([Arel.sql('field(id, ?)'), list_product_ids]).
+      includes(master: [:default_price, :images])
   end
 end
