67137979ffeb5114593b25ffaf46d41d197af5aa
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-21 20:18:42 +0900

    関連商品取得をmodelメソッドに切り出し

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,21 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @products = list_up_related_products(@product)
-  end
-
-  private
-
-  def list_up_related_products(origin_product)
-    list_taxonomy_ids = Spree::Taxonomy.all.order(:position).ids
-    list_taxons = origin_product.taxons.
-      order([Arel.sql('field(taxonomy_id, ?)'), list_taxonomy_ids]).includes(:products)
-    list_product_ids = []
-    list_taxons.each do |list_taxon|
-      list_product_ids.concat(list_taxon.products.ids.reverse!)
-    end
-    list_product_ids.uniq!&.delete(origin_product.id)
-    Spree::Product.where(id: list_product_ids).order([Arel.sql('field(id, ?)'), list_product_ids]).
-      includes(master: [:default_price, :images])
+    @products = @product.list_up_relations.includes(master: [:default_price, :images])
   end
 end
---
+++ app/models/potepan/product_decorator.rb
@@ -0,0 +1,19 @@
+module Potepan::ProductDecorator
+  def list_up_relations
+    list_taxonomy_ids = Spree::Taxonomy.all.order(:position).ids
+    list_taxons = taxons.order([Arel.sql('field(taxonomy_id, ?)'), list_taxonomy_ids])
+    list_product_ids = []
+    list_taxons.each do |list_taxon|
+      list_product_ids.concat(list_taxon.products.ids.reverse!)
+      list_product_ids.uniq!
+      list_product_ids.delete(id)
+      if list_product_ids.length >= 4
+        list_product_ids = list_product_ids[0..3]
+        break
+      end
+    end
+    Spree::Product.where(id: list_product_ids).order([Arel.sql('field(id, ?)'), list_product_ids])
+  end
+
+  Spree::Product.prepend self
+end
