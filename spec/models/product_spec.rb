4ab1cda91e75381f8e6b5817d22dae89caeec802
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-21 20:27:41 +0900

    modelテスト（list_up_relations）の追加

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe "Potepan::Product", type: :model do
+  let(:taxonomy_a) { create(:taxonomy, position: 1) }
+  let(:taxonomy_b) { create(:taxonomy, position: 2) }
+  let(:taxon_a) do
+    create(:taxon, name: 'taxon_a', parent_id: taxonomy_a.root.id, taxonomy: taxonomy_a)
+  end
+  let(:taxon_b) do
+    create(:taxon, name: 'taxon_b', parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)
+  end
+  let(:taxon_not_related) do
+    create(:taxon, name: 'taxon_not_related', parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)
+  end
+  let(:product_with_taxon) do
+    create(:product, name: 'product_with_taxon', taxons: [taxon_a, taxon_b])
+  end
+  let(:product_without_taxon) { create(:product, name: 'product_without_taxon') }
+  let!(:product_id1_related_a) do
+    create(:product, id: 1, name: 'product_id1_related_a', taxons: [taxon_a])
+  end
+  let!(:product_id2_related_b) do
+    create(:product, id: 2, name: 'product_id2_related_b', taxons: [taxon_b])
+  end
+  let!(:product_id3_related_a_b) do
+    create(:product, id: 3, name: 'product_id3_related_a_b', taxons: [taxon_a, taxon_b])
+  end
+  let!(:product_id4_related_b) do
+    create(:product, id: 4, name: 'product_id4_related_b', taxons: [taxon_b])
+  end
+  let!(:product_id5_related_b) do
+    create(:product, id: 5, name: 'product_id5_related_b', taxons: [taxon_b])
+  end
+  let!(:product_not_related) do
+    create(:product, name: 'product_not_rerated', taxons: [taxon_not_related])
+  end
+
+  describe '関連商品を抽出する機能' do
+    context '商品が関連商品を4つ以上持つ場合' do
+      it '関連商品のみ4つ順番通りに取得する' do
+        relations = product_with_taxon.list_up_relations
+        expect(relations.length).to eq 4
+        expect(relations[0]).to eq product_id3_related_a_b
+        expect(relations[1]).to eq product_id1_related_a
+        expect(relations[2]).to eq product_id5_related_b
+        expect(relations[3]).to eq product_id4_related_b
+      end
+    end
+
+    context '商品が関連商品を持たない場合' do
+      it '商品情報は取得されない' do
+        expect(product_without_taxon.list_up_relations).to eq []
+      end
+    end
+  end
+end
