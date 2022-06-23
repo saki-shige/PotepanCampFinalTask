3130516a88e4875c41080edb472e316b31d45d49
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-23 14:21:51 +0900

    subjectを使用して記述を共通化、無駄な記述削除

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,33 +9,28 @@ RSpec.describe "Potepan::Product", type: :model do
   let(:taxon_b) do
     create(:taxon, name: 'taxon_b', parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)
   end
-  let(:product_with_taxon) do
-    create(:product, name: 'product_with_taxon', taxons: [taxon_a, taxon_b])
-  end
-  let(:product_without_taxon) { create(:product, name: 'product_without_taxon') }
-  let!(:product_id1_related_a) do
-    create(:product, id: 1, name: 'product_id1_related_a', taxons: [taxon_a])
-  end
-  let!(:product_id2_related_b) do
-    create(:product, id: 2, name: 'product_id2_related_b', taxons: [taxon_b])
-  end
-  let!(:product_id3_related_a_b) do
-    create(:product, id: 3, name: 'product_id3_related_a_b', taxons: [taxon_a, taxon_b])
-  end
-  let!(:product_id4_related_b) do
-    create(:product, id: 4, name: 'product_id4_related_b', taxons: [taxon_b])
-  end
+  let(:product) { create(:product, taxons: taxons) }
+  let!(:product_id1_related_a) { create(:product, id: 1, taxons: [taxon_a]) }
+  let!(:product_id2_related_b) { create(:product, id: 2, taxons: [taxon_b]) }
+  let!(:product_id3_related_a_b) { create(:product, id: 3, taxons: [taxon_a, taxon_b]) }
+  let!(:product_id4_related_b) { create(:product, id: 4, taxons: [taxon_b]) }
 
   describe '関連商品を抽出する機能' do
+    subject { product.list_up_relations(limit: 3) }
+
     context '商品が関連商品を指定数以上持つ場合' do
+      let(:taxons) { [taxon_a, taxon_b] }
+
       it '関連商品のみを指定数順番通りに取得する' do
-        expect(product_with_taxon.list_up_relations(limit: 3)).to eq [product_id3_related_a_b, product_id1_related_a, product_id4_related_b]
+        is_expected.to eq [product_id3_related_a_b, product_id1_related_a, product_id4_related_b]
       end
     end
 
     context '商品が関連商品を持たない場合' do
+      let(:taxons) { [] }
+
       it '商品情報は取得されない' do
-        expect(product_without_taxon.list_up_relations(limit: 3)).to eq []
+        is_expected.to eq []
       end
     end
   end
