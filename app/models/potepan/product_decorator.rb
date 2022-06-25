9c9de8abcd87d320a2b4ed905c73eb1b667f9827
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-25 18:15:22 +0900

    Arelを削除

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,18 +1,17 @@
 module Potepan::ProductDecorator
-  def list_up_relations(limit: i)
-    list_taxonomy_ids = Spree::Taxonomy.all.order(:position).ids
-    list_taxons = taxons.order([Arel.sql('field(taxonomy_id, ?)'), list_taxonomy_ids])
+  def list_up_relations(limit:)
+    list_taxons = taxons.joins(:taxonomy).order('spree_taxonomies.position asc')
     list_product_ids = []
     list_taxons.each do |list_taxon|
       list_product_ids.concat(list_taxon.products.ids.reverse!)
       list_product_ids.uniq!
       list_product_ids.delete(id)
       if list_product_ids.length >= limit
-        list_product_ids = list_product_ids[0..(limit-1)]
+        list_product_ids = list_product_ids[0..(limit - 1)]
         break
       end
     end
-    Spree::Product.where(id: list_product_ids).order([Arel.sql('field(id, ?)'), list_product_ids])
+    Spree::Product.find(list_product_ids).sort_by { |x| list_product_ids }
   end
 
   Spree::Product.prepend self
