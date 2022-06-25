47bb1344d164d98dabe3715fc05b80f761b2472d
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-25 18:44:32 +0900

    記述を短く修正

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -1,17 +1,10 @@
 module Potepan::ProductDecorator
   def list_up_relations(limit:)
-    list_taxons = taxons.joins(:taxonomy).order('spree_taxonomies.position asc')
-    list_product_ids = []
-    list_taxons.each do |list_taxon|
-      list_product_ids.concat(list_taxon.products.ids.reverse!)
-      list_product_ids.uniq!
-      list_product_ids.delete(id)
-      if list_product_ids.length >= limit
-        list_product_ids = list_product_ids[0..(limit - 1)]
-        break
-      end
-    end
-    Spree::Product.find(list_product_ids).sort_by { |x| list_product_ids }
+    Spree::Product.joins(taxons: :taxonomy).
+      where.not(id: id).
+      where(spree_taxons: { id: taxons.ids }).
+      order('spree_taxonomies.position asc').
+      order(id: :desc).uniq[0..(limit - 1)]
   end
 
   Spree::Product.prepend self
