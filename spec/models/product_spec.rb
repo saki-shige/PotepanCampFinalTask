4dcafbc92d50318ff46a2d35f807cc0e3fda97e9
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-27 17:20:22 +0900

    関連していない商品を追加

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 RSpec.describe "Potepan::Product", type: :model do
   let(:taxonomy_a) { create(:taxonomy, position: 1) }
   let(:taxonomy_b) { create(:taxonomy, position: 2) }
-  let(:taxon_a) do
-    create(:taxon, name: 'taxon_a', parent_id: taxonomy_a.root.id, taxonomy: taxonomy_a)
-  end
-  let(:taxon_b) do
-    create(:taxon, name: 'taxon_b', parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)
-  end
+  let(:taxon_a) {create(:taxon, parent_id: taxonomy_a.root.id, taxonomy: taxonomy_a)}
+  let(:taxon_b) {create(:taxon, parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)}
+  let(:taxon_c) {create(:taxon, parent_id: taxonomy_b.root.id, taxonomy: taxonomy_b)}
   let(:product) { create(:product, taxons: taxons) }
   let!(:product_id1_related_a) { create(:product, id: 1, taxons: [taxon_a]) }
   let!(:product_id2_related_b) { create(:product, id: 2, taxons: [taxon_b]) }
   let!(:product_id3_related_a_b) { create(:product, id: 3, taxons: [taxon_a, taxon_b]) }
+  let!(:product_id4_related_c) { create(:product, id: 4, taxons: [taxon_c]) }
 
   describe 'list_up_relations' do
     subject { product.list_up_relations }
