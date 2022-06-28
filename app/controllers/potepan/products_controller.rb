924d3565c2e4a0b297ab9c0e5f7570ced6656b18
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-28 20:38:20 +0900

    定数名を変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,10 +1,10 @@
 class Potepan::ProductsController < ApplicationController
-  LIMIT_FOR_DISPLAY = 4
+  MAX_RELATED_PRODUCTS_COUNT = 4
 
   def show
     @product = Spree::Product.find(params[:id])
     @products = @product.list_up_relations.
       includes(master: [:default_price, :images]).
-      limit(LIMIT_FOR_DISPLAY)
+      limit(MAX_RELATED_PRODUCTS_COUNT)
   end
 end
