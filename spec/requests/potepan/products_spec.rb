7e51af1d7d8facb05d98f14ba310be702e1c9185
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-06-30 12:00:16 +0900

    無駄なテストデータを削除

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,38 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
-  let(:taxonomy_a) { create(:taxonomy, position: 1) }
-  let(:taxonomy_b) { create(:taxonomy, position: 2) }
-  let(:taxon_a) do
-    create(:taxon, name: 'taxon_a', parent_id: taxonomy_a.root.id,
-                   taxonomy: taxonomy_a)
-  end
-  let(:taxon_b) do
-    create(:taxon, name: 'taxon_b', parent_id: taxonomy_b.root.id,
-                   taxonomy: taxonomy_b)
-  end
-  let!(:taxon_not_related) do
-    create(:taxon, name: 'taxon_not_related', parent_id: taxonomy_b.root.id,
-                   taxonomy: taxonomy_b)
-  end
-  let!(:product_with_taxon) do
-    create(:product, name: 'product_with_taxon', taxons: [taxon_a, taxon_b])
-  end
-  let!(:product_id1_related_a) do
-    create(:product, id: 1, name: 'product_id1_related_a', taxons: [taxon_a])
-  end
-  let!(:product_id2_related_b) do
-    create(:product, id: 2, name: 'product_id2_related_b', taxons: [taxon_b])
-  end
-  let!(:product_id3_related_a_b) do
-    create(:product, id: 3, name: 'product_id3_related_a_b', taxons: [taxon_a, taxon_b])
-  end
-  let!(:product_not_related) do
-    create(:product, name: 'product_not_rerated', taxons: [taxon_not_related])
-  end
+  let(:taxonomy) { create(:taxonomy) }
+  let(:taxon) { create(:taxon, parent_id: taxonomy.root.id, taxonomy: taxonomy) }
+  let!(:other_taxon) { create(:taxon, parent_id: taxonomy.root.id, taxonomy: taxonomy) }
+  let!(:product) { create(:product, name: 'product', taxons: [taxon]) }
+  let!(:related_product) { create(:product, name: 'related_product', taxons: [taxon]) }
+  let!(:other_product) { create(:product, name: 'product_not_rerated', taxons: [other_taxon]) }
 
   before do
-    get potepan_product_path(product_with_taxon.id)
+    get potepan_product_path(product.id)
   end
 
   describe '詳細ページテスト' do
@@ -41,25 +18,21 @@ RSpec.describe "Potepan::Products", type: :request do
     end
 
     it '商品名が表示される' do
-      expect(response.body).to include(product_with_taxon.name)
+      expect(response.body).to include(product.name)
     end
 
     it '値段が表示される' do
-      expect(response.body).to include(product_with_taxon.display_price.to_s)
+      expect(response.body).to include(product.display_price.to_s)
     end
 
     it '説明が表示される' do
-      expect(response.body).to include(product_with_taxon.description)
+      expect(response.body).to include(product.description)
     end
 
     it '関連商品が表示される' do
-      expect(response.body).to include(product_id1_related_a.name)
-      expect(response.body).to include(product_id1_related_a.display_price.to_s)
-      expect(response.body).to include(product_id2_related_b.name)
-      expect(response.body).to include(product_id2_related_b.display_price.to_s)
-      expect(response.body).to include(product_id3_related_a_b.name)
-      expect(response.body).to include(product_id3_related_a_b.display_price.to_s)
-      expect(response.body).not_to include(product_not_related.name)
+      expect(response.body).to include(related_product.name)
+      expect(response.body).to include(related_product.display_price.to_s)
+      expect(response.body).not_to include(other_product.name)
     end
   end
 end
